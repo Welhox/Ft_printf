b6819bfd03409c40d3e0992bb9f40ebec69f28d5
Casimir Lundberg <clundber@c1r4p3.hive.fi>
2023-11-13 14:27:41 +0200

    basic printing works

--- ft_printf.c
+++ ft_printf.c
@@ -6,40 +6,76 @@
 /*   By: clundber <clundber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 15:59:15 by clundber          #+#    #+#             */
-/*   Updated: 2023/11/10 18:09:11 by clundber         ###   ########.fr       */
+/*   Updated: 2023/11/13 14:01:15 by clundber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
+#include "libftprintf.h"
+
+static int ft_puthex(long nbr, int caps)
+
+{
+	int		count;
+	char	*hex;
+
+	count = 0;
+	if (caps == 0)
+		hex = "0123456789abcdef";
+	else
+		hex = "0123456789ABCDEF";
+	if (nbr < 0)
+	{
+		write(1, "-", 1);
+		nbr *= -1;
+		count++;
+	}
+	else if (nbr > 16)
+	{
+		ft_puthex((nbr /= 16), caps);
+		ft_putchar_fd(hex[nbr % 16], 1);
+		//write(1, &hex[nbr % 16], 1);
+		return (count);
+	}
+		else
+	{
+		ft_putchar_fd(hex[nbr], 1);
+		count++;
+		return (count);
+	}
 
 
-static void ft_print_special(const char s, va_list ap)
+
+	return (count);
+}
+
+static int ft_print_special(const char s, va_list ap)
 
 {
 	int	count;
 
 	count = 0;
 	if (s == 'c')
-		count += ft_putchar_fd(ap, char, 1);
-	else if (s == s)
-		count += ft_putstr(ap, char*, 1);
-	else if (s == d)
-		count += ft_putnbr_fd(ap, int, 1);  //should be float?
-	else if (s == i)
-		count += ft_putnbr_fd(ap, int, 1);
-	else if (s == u)
-		count += ft_putnbr_fd(ap, int, 1); //unsigned decimal
-	else if (s == x)
-		count += ft_puthex(ap, int); //lowercase hex
-	else if (s == X)
-		count += ft_puthex(ap, int); //uppercase hex
-	else if (s == %)
-			count += ft_putchar_fd(ap, char, 1);
+		ft_putchar_fd(va_arg(ap, int), 1);
+	else if (s == 's')
+		ft_putstr_fd(va_arg(ap, char*), 1);
+	else if (s == 'd')
+		ft_putnbr_fd(va_arg(ap, int), 1);  //should be float?
+	else if (s == 'i')
+		ft_putnbr_fd(va_arg(ap, int), 1);
+	else if (s == 'u')
+		ft_putnbr_fd(va_arg(ap, unsigned int), 1);
+	else if (s == 'x')
+		count += ft_puthex(va_arg(ap, long), 0); //lowercase hex
+	else if (s == 'X')
+		count += ft_puthex(va_arg(ap, long), 1); //uppercase hex
+	//else if (s == '%') */
+	//	ft_putchar_fd(ap, s, 1); 
 	else
-		count += write(1, &s, 1);
+		write(1, &s, 1);
 	return (count);
 }
 int	ft_printf (const char *str, ...)
@@ -50,15 +86,15 @@ int	ft_printf (const char *str, ...)
 
 	va_start(ap, str);
 	counter = 0;
-	while(*str)
+	while(str[counter])
 	{
-		if (*str == %)
+		if (str[counter] == '%')
 		{
-			count++;
-			count += ft_print_special(*s, ap);
+			counter++;
+			counter += ft_print_special(str[counter], ap);
 		}
 		else
-			write(1, str, 1);
+			write(1, &str[counter], 1);
 		counter++;
 	}
 	va_end(ap);
